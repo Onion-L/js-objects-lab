24f7bee3db8433fa94827c9907deff440cf9abf7
Onion-L <lixiang020317@gmail.com>
2023-09-12 10:46:02 +0100

    Arrays.

--- fundamental.js
+++ fundamental.js
@@ -11,6 +11,7 @@ const aCar = {
         model: 'Corolla',
         cc:'1.8'
     },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration: { year: 201, county: "WD", number: 1058 },
     mileage: 10000,
     colour:{
@@ -31,3 +32,11 @@ const aCar = {
   console.log(`It is a ${aCar.colour.exterior} car, ${aCar.mileage} mileage, with ${aCar.colour.interior.texture} interior.`);
 
   console.log('First owner : ' + aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address);
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+}
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name) ;
+}
