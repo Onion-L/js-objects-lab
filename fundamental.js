f96100514e04eda76f75a5e315aaa260a87e3e71
Onion-L <lixiang020317@gmail.com>
2023-09-12 10:20:38 +0100

    Nested objects.

--- fundamental.js
+++ fundamental.js
@@ -1,7 +1,15 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    registration : '201WD1058'
+    type : {
+        make:'Toyota',
+        model: 'Corolla',
+        cc:'1.8'
+    },
+    registration: { year: 201, county: "WD", number: 1058 }
   };
   
   console.log(aCar.owner);
+  console.log(aCar.owner + ' drives a ' + aCar.type.make);
+  
+  //Reg. = 201-WD-1058.
+  console.log(`Reg. = ${aCar.registration.year}-${aCar.registration.county}-${aCar.registration.number}`);
